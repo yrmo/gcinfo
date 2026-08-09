3514f45eab666b2b7ac285398b419af93b400189
Ryan Moore <ryanm.inbox@gmail.com>
2026-08-09 14:20:18 -0400

    Add alternative url

--- gcinfo.py
+++ gcinfo.py
@@ -1,6 +1,7 @@
 import requests
 
 url = "https://open.canada.ca/data/dataset/a35cf382-690c-4221-a971-cf0fd189a46f/resource/64774bc1-c90a-4ae2-a3ac-d9b50673a895/download/rbpo_rppo_en.csv"
+url2 = "https://open.canada.ca/data/dataset/b15ee8d7-2ac0-4656-8330-6c60d085cda8/resource/02929919-d9ab-494e-8fce-012119b479ff/download/rbpo_rppo_en.csv"
 
 r = requests.get(url, allow_redirects=True, timeout=60)
 r.raise_for_status()
